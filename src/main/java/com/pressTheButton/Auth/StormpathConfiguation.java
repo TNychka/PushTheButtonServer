9f9ab937ff113de6b47c3703f90e10414c81f6c8
Tyler Nychka <tyler.nychka@Gmail.com>
2016-08-20 21:43:26 -0400

    Update to the GameUtils and Gamecontroller endpoints

--- src/main/java/com/pressTheButton/Auth/StormpathConfiguation.java
+++ src/main/java/com/pressTheButton/Auth/StormpathConfiguation.java
@@ -1,5 +1,6 @@
 package com.pressTheButton.Auth;
 
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -7,16 +8,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Tyler on 2016-08-14.
  */
-@Configuration
-@EnableAutoConfiguration
-public class StormpathConfiguation {
 
-    public StormpathConfiguation(){
-    }
+@Component
+@NoArgsConstructor
+public class StormpathConfiguation {
 
     @Bean
     public StormpathApp stormpathApp(){
